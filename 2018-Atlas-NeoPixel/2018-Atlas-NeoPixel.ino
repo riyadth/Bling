cd60fc3fd73f00f38d74b1393c5febc437277711
Riyadth Al-Kazily <riyadth@gmail.com>
2018-06-09 23:57:08 -0700

    Fix for refactored code so macro works

--- 2018-Atlas-NeoPixel/2018-Atlas-NeoPixel.ino
+++ 2018-Atlas-NeoPixel/2018-Atlas-NeoPixel.ino
@@ -1,20 +1,24 @@
 #include <Adafruit_NeoPixel.h>
 #include <ctype.h>
 
-// Colors
-#define OFF     Adafruit_NeoPixel::Color(0,0,0)         //actually a dim white
-#define BLUE    Adafruit_NeoPixel::Color(0, 0, 128)
-#define RED     Adafruit_NeoPixel::Color(128, 0, 0)
-#define YELLOW  Adafruit_NeoPixel::Color(128, 128, 0)
-#define GREEN   Adafruit_NeoPixel::Color(0, 128, 0)
-#define PURPLE  Adafruit_NeoPixel::Color(128,0,128)
-
 // Configuration for our LED strip and Arduino
 #define NUM_LEDS 150
 #define PIN 11
 
 #define FADE_MAX_BRIGHTNESS     100
 #define FLASH_TIME_INTERVAL     100
+
+// allocate our pixel memory, set interface to match our hardware
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+// Colors
+const uint32_t OFF     = pixels.Color(0,0,0);
+const uint32_t RED     = pixels.Color(128, 0, 0);
+const uint32_t GREEN   = pixels.Color(0, 128, 0);
+const uint32_t BLUE    = pixels.Color(0, 0, 128);
+const uint32_t YELLOW  = pixels.Color(128, 128, 0);
+const uint32_t PURPLE  = pixels.Color(128,0,128);
+
 
 // Commands
 #define COMMAND_RESET           '8'
@@ -34,22 +38,26 @@ enum {
 
 uint32_t blingColor = OFF;
 int blingEffect = SOLID;
-int blingBrightness = 100;
+uint8_t blingBrightness = 100;
 
-// allocate our pixel memory, set interface to match our hardware
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB+NEO_KHZ800);
+
 
 
 // "Magic" delay macro to exit a function if a new command is available
 // Execute the delay 10ms at a time, and check for available bytes
 #define INTERRUPTABLE_DELAY(x)  {                               \
-  int timeInterval = (x);                                       \
-  while (timeInterval > 0) {                                    \
-    if (checkForCommand()) {                                    \
+  unsigned long timeIntervalCounter = (x);                      \
+  while (timeIntervalCounter != 0) {                            \
+    if (timeIntervalCounter > 10) {                             \
+      delay(10);                                                \
+      timeIntervalCounter = timeIntervalCounter - 10;           \
+    } else {                                                    \
+      delay(timeIntervalCounter);                               \
+      timeIntervalCounter = 0;                                  \
+    }                                                           \
+    if (checkForCommand() == true) {                            \
       return;                                                   \
     }                                                           \
-    delay(min(10, timeInterval));                               \
-    timeInterval = timeInterval - min(10, timeInterval);        \
   }                                                             \
 }
 
@@ -61,7 +69,7 @@ bool checkForCommand(void)
 {
   bool result = false;
   char newCommand;
-
+  
   // Preset our "new command" variable to be the same as the current command
   newCommand = currentCommand;
   // If there are any characters available on the serial port, read them
@@ -92,7 +100,7 @@ bool checkForCommand(void)
 // Fill the strip with a single color
 void fillStrip(uint32_t c)
 {
-   for (int i=0; i<NUM_LEDS; i++)
+   for (unsigned i=0; i<pixels.numPixels(); i++)
    {
       pixels.setPixelColor(i, c);
    }
@@ -104,7 +112,7 @@ void fillStrip(uint32_t c)
  ******************************************************************************/
 
 // Solid illumination of a single color
-void solid(int color, int brightness)
+void solid(uint32_t color, uint8_t brightness)
 {
   Serial.println("solid()");
   pixels.setBrightness(brightness);
@@ -116,17 +124,18 @@ void solid(int color, int brightness)
 }
 
 // Flash of light for a short period of time, then off (runs once only)
-void flash(int timemInterval, int color, int brightness)
+void flash(unsigned long timeInterval, uint32_t color, uint8_t brightness)
 {
   Serial.println("flash()");
   pixels.setBrightness(brightness);
   fillStrip(color);
-  INTERRUPTABLE_DELAY(timeInterval);
+  delay(timeInterval);
+  Serial.println("Done with flash");
   currentCommand = COMMAND_RESET;
 }
 
 // Blink a single color on and off at the given time interval
-void blink(int timeInterval, int color, int brightness)
+void blink(unsigned long timeInterval, uint32_t color, uint8_t brightness)
 {
   Serial.println("blink()");
   pixels.setBrightness(brightness);
@@ -140,10 +149,10 @@ void blink(int timeInterval, int color, int brightness)
 }
 
 // Fade a color in and out
-void fade(int timeInterval, int color, int maxBrightness)
+void fade(unsigned long timeInterval, uint32_t color, uint8_t maxBrightness)
 {
   bool brightening = true;
-  int brightness = 0;
+  uint8_t brightness = 0;
 
   Serial.println("fade()");
   // Set the strip to the desired color to begin with
@@ -180,11 +189,11 @@ void fade(int timeInterval, int color, int maxBrightness)
 }
 
 // Fade between two alternating colors
-void spartronics_fade(int timeInterval, int color1, int color2, int maxBrightness)
+void spartronics_fade(unsigned long timeInterval, uint32_t color1, uint32_t color2, uint8_t maxBrightness)
 {
   Serial.println("spartronics_fade()");
   bool brightening = true;
-  int brightness = 0;
+  uint8_t brightness = 0;
   bool color1_is_current = true;;
 
   // Set the strip to the desired color to begin with
@@ -234,7 +243,7 @@ void spartronics_fade(int timeInterval, int color1, int color2, int maxBrightnes
 
 
 // Classic Spartronics Cogs
-void cogs(int timeInterval, int color1, int color2)
+void cogs(unsigned long timeInterval, uint32_t color1, uint32_t color2)
 {
   Serial.println("cogs");
   while (1)
@@ -259,7 +268,7 @@ void setup() {
 
 void loop()
 {
-  int effectDelay = 10; // Default effect delay
+  unsigned long effectDelay = 10; // Default effect delay
 
   // Take the current command and set the parameters
   switch (currentCommand)
@@ -279,12 +288,12 @@ void loop()
       break;
     case '9':
       blingEffect = SLOW_BLINK;
-      effectDelay = 500;
+      effectDelay = 1500;
       break;
     case 'f':
       blingEffect = FLASH;
       blingColor = RED;
-      effectDelay = 750;
+      effectDelay = 100;
       break;
     case 'c':
       blingEffect = COGS;
@@ -329,6 +338,7 @@ void loop()
       effectDelay = 10;
       blingBrightness = 100;
       pixels.setBrightness(blingBrightness);
+      pixels.clear();
   }
 
   // Now we run the blingEffect selected until the next command is sent
