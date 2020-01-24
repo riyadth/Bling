cb8d4876010b7a6a4513a2f84f1308a457748282
Riyadth Al-Kazily <riyadth@gmail.com>
2020-01-23 21:05:42 -0800

    Added cogs code from original Atlas
    Also set default brightness to 32 for all commands except flash

--- 2020-Atlas-NeoPixel/2020-Atlas-NeoPixel.ino
+++ 2020-Atlas-NeoPixel/2020-Atlas-NeoPixel.ino
@@ -9,6 +9,8 @@
 // Time in milliseconds for the flash of light
 #define FLASH_TIME_INTERVAL     250
 
+#define DEFAULT_BRIGHTNESS      32
+
 // allocate our pixel memory, set interface to match our hardware
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -317,6 +319,84 @@ void cogs_init(uint32_t color1, uint32_t color2)
   pixels.show();
 }
 
+//moves every led up one, and inserts colorIn at leds[0]
+void shiftUp(uint32_t colorIn) {
+  for (int i = (NUM_LEDS - 1); i > 0; i--) {
+    //The last led becomes the previous led's color
+    pixels.setPixelColor(i, pixels.getPixelColor(i-1));
+  }
+  pixels.setPixelColor(0, colorIn);
+}
+
+//moves every led down one, and inserts colorIn at leds[(NUM_LEDS - 1)]
+void shiftDown(uint32_t colorIn) {
+  for (int i = 0; i < (NUM_LEDS - 1); i++) {
+    //The first led becomes the next led's color
+    pixels.setPixelColor(i, pixels.getPixelColor(i+1));
+  }
+  pixels.setPixelColor(NUM_LEDS-1, colorIn);
+}
+
+void moveCogsUp() {
+  boolean changeColor = true;
+  for (int i = 0; i < (COG_SIZE - 1); i++) {
+    if (pixels.getPixelColor(i) != pixels.getPixelColor(i+1)) {
+      changeColor = false;
+    }
+  }
+  if (changeColor) {
+    shiftUp(pixels.getPixelColor(COG_SIZE));
+  }
+  else {
+    shiftUp(pixels.getPixelColor(0));
+  }
+}
+
+void moveCogsDown() {
+  boolean changeColor = true;
+  for (int i = (NUM_LEDS - 1); i > (NUM_LEDS - (COG_SIZE)); i--) {
+    if (pixels.getPixelColor(i) != pixels.getPixelColor(i-1)) {
+      changeColor = false;
+    }
+  }
+  if (changeColor) {
+    shiftDown(pixels.getPixelColor(NUM_LEDS - COG_SIZE - 1));
+  }
+  else {
+    shiftDown(pixels.getPixelColor(NUM_LEDS - 1));
+  }
+}
+
+void rotatingCogsUp(uint32_t colorOne, uint32_t colorTwo) {
+  pixels.show();
+  for (int i = 0; i < 30; i++) {
+    for (int c = 0; c < COG_SIZE; c++) {
+      moveCogsUp();
+      _delay(100);
+      pixels.show();
+    }
+  }
+}
+
+void rotatingCogsDown(uint32_t colorOne, uint32_t colorTwo) {
+  pixels.show();
+  for (int i = 0; i < 30; i++) {
+    for (int c = 0; c < COG_SIZE; c++) {
+      moveCogsDown();
+      _delay(100);
+      pixels.show();
+    }
+  }
+}
+
+void cogs(uint32_t color1, uint32_t color2)
+{
+  cogs_init(color1, color2);
+  while (1) {
+    rotatingCogsUp(color1, color2);
+    rotatingCogsDown(color1, color2);
+  }
+}
 
 /*******************************************************************************
  * Arduino setup() and loop()
@@ -325,7 +405,7 @@ void cogs_init(uint32_t color1, uint32_t color2)
 void setup(void) {
   Serial.begin(9600);
   pixels.begin(); // initialize neopixel library
-  pixels.setBrightness(64);
+  pixels.setBrightness(DEFAULT_BRIGHTNESS);
   fillStrip(ORANGE);
 }
 
@@ -347,13 +427,13 @@ void loop(void)
   {
     // Bling effects
     case OFF:
-      solid(NONE, 255);
+      solid(NONE, DEFAULT_BRIGHTNESS);
       break;
     case DISABLED:
-      solid(ORANGE, 255);
+      solid(ORANGE, DEFAULT_BRIGHTNESS);
       break;
     case INTAKE_UP:
-      cogs_init(BLUE, YELLOW);
+      cogs(BLUE, YELLOW);
       wait_for_command();
       break;
     case INTAKE_DOWN:
